b889616673f39a93a3978f39d996f0acb4898058
Simoweb <nicolas-simonet@live.fr>
2022-04-21 01:37:41 +0200

    maj responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import Logo from "../img/logo.png";
 
 const Navigation = () => {
-  
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
-    <nav className="navigation">
-      <ul>
-      <NavLink 
-        to="/">
-        <img className="logo-nav" src="./logo.png" alt="logo dawn valley" />
+    <nav className={ isNavExpanded ? "navigation expanded" : "navigation"}>
+      <NavLink to="/">
+        <img className="logo-nav" src={Logo} alt="logo dawn valley" />
       </NavLink>
-        <NavLink 
-          to="/" 
-          className={(nav) => (nav.isActive ? "nav-active" : "")}
-          >
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="black"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <ul>
+        <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
           <li>Accueil</li>
         </NavLink>
         <NavLink
