392c7fda009b265393a7edef25206d9ee93d0639
Simoweb <nicolas-simonet@live.fr>
2022-04-21 13:26:30 +0200

    footer img opti fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
   return (
     <footer>
       <ul>
-        <li><a href="https://www.instagram.com/dawnvalleyredm/" target="_blank" rel='noopener noreferrer'><img src={Insta} alt="" />.</a></li>
-        <li><a href="https://twitter.com/DawnvalleyR" target="_blank" rel='noopener noreferrer'><img src={Twitter} alt="" />.</a></li>
-        <li><a href="https://www.youtube.com/channel/UCqZDD2vqegSmeyIALgGg_nQ" target="_blank" rel='noopener noreferrer'>.<img src={Youtube} alt="" /></a></li>
-        <li><a href="hhttps://discord.gg/82dfKfXa6D" target="_blank" rel='noopener noreferrer'><img src={Discord} alt="" />.</a></li>
+        <li><a href="https://www.instagram.com/dawnvalleyredm/" target="_blank" rel='noopener noreferrer'><img src={Insta} width="40" height="40" alt="" />.</a></li>
+        <li><a href="https://twitter.com/DawnvalleyR" target="_blank" rel='noopener noreferrer'><img src={Twitter} width="40" height="40" alt="" />.</a></li>
+        <li><a href="https://www.youtube.com/channel/UCqZDD2vqegSmeyIALgGg_nQ" target="_blank" rel='noopener noreferrer'>.<img src={Youtube} width="40" height="40" alt="" /></a></li>
+        <li><a href="hhttps://discord.gg/82dfKfXa6D" target="_blank" rel='noopener noreferrer'><img src={Discord} width="40" height="40" alt="" />.</a></li>
       </ul>
     </footer>
   );
